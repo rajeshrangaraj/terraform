91db590efa9912378ac49c4b6206e1776b5634fe
Rajesh <rajesh.rangaraj@gmail.com>
2026-05-31 22:30:54 +0100

    fix: terraform fmt formatting

--- ec2/infra/outputs.tf
+++ ec2/infra/outputs.tf
@@ -4,7 +4,7 @@
 
 output "debug_info" {
   value = {
-    region        = module.app_server.aws_region
+    region = module.app_server.aws_region
     # account_id    = module.app_server.aws_caller_identity
     # aws_partition = module.app_server.aws_partition
     # instance_id   = module.app_server.instance_id
