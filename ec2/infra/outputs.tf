5d5920f9905d5c39f42199c599c0d4d003b7dadf
Rajesh <rajesh.rangaraj@gmail.com>
2026-05-31 22:50:18 +0100

    fix: terraform added few more variables

--- ec2/infra/outputs.tf
+++ ec2/infra/outputs.tf
@@ -4,10 +4,10 @@
 
 output "debug_info" {
   value = {
-    region = module.app_server.aws_region
-    # account_id    = module.app_server.aws_caller_identity
-    # aws_partition = module.app_server.aws_partition
-    # instance_id   = module.app_server.instance_id
+    region        = module.app_server.aws_region
+    account_id    = module.app_server.aws_caller_identity
+    aws_partition = module.app_server.aws_partition
+    instance_id   = module.app_server.instance_id
 
   }
 }
