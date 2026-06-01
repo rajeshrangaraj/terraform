45faf5fbbce2054fcbf7ca75ba4399fe74b9aff8
Rajesh <rajesh.rangaraj@gmail.com>
2026-06-01 22:08:30 +0100

    fix: added secret manager -10

--- ec2/infra/outputs.tf
+++ ec2/infra/outputs.tf
@@ -8,9 +8,17 @@ output "debug_info" {
     account_id    = module.app_server.aws_caller_identity
     aws_partition = module.app_server.aws_partition
     instance_id   = module.app_server.instance_id
-    username      = module.app_server.db_username
-    password      = module.app_server.db_password
-
-
   }
+}
+
+output "db_username" {
+  description = "Database username"
+  value       = module.app_server.db_username
+  sensitive   = true
+}
+
+output "db_password" {
+  description = "Database password"
+  value       = module.app_server.db_password
+  sensitive   = true
 }
